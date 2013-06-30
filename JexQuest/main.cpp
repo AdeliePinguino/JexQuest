614d0dd96ef2254f28bd9f4f4d9211aa32e664b4
April Paul <aprilpaul@letu.edu>
2013-06-30 17:22:47 -0500

    First Upload

---
+++ JexQuest/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int b;
+
+    cout << "Welcome to Jex Quest: The Magical Adventure of the Elven Maiden, Jex!" << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Press any # to begin." << endl;
+    cin >> b;
+
+
+    return 0;
+}
+
